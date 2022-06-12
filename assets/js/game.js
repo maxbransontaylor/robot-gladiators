8b4913d65623af738a7ee7a9cde9335f08138eb4
Max Taylor <maxbransontaylor@gmail.com>
2022-06-12 18:23:59 -0400

    testing console logs

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("Hello world!")
+// one line comment
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+console.log("This logs a string");
+console.log(10 + 10)
+console.log("our robot's name is " + playerName);
+
+function fight() {
+    window.alert("The fight has begun!")
+}
+//fight();
