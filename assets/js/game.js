82ee15737dd97f2bb9418aeb47755c1f8789f95f
Max Taylor <maxbransontaylor@gmail.com>
2022-06-17 17:56:17 -0400

    bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -58,19 +58,32 @@ var enemyInfo = [
   },
 ];
 
+var fightOrSkip = function () {
+  var promptFight = window.prompt(
+    "Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose."
+  );
+
+  if (!promptFight) {
+    window.alert("You need to provide a valid answer! Please try again.");
+    return fightOrSkip();
+  }
+  promptFight = promptFight.toLowerCase();
+  if (promptFight === "skip") {
+    var confirmSkip = window.confirm("Are you sure you want to skip?");
+    if (confirmSkip) {
+      window.alert(playerInfo.name + " has choosen to skip the fight!");
+      playerInfo.money = Math.max(0, playerInfo.money - 10);
+      console.log("playerInfo.money", playerInfo.money);
+      return true;
+    }
+  }
+  return false;
+};
+
 var fight = function (enemy) {
   while (enemy.health > 0 && playerInfo.health > 0) {
-    var promptFight = window.prompt(
-      "Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose."
-    );
-    if (promptFight === "skip" || promptFight === "SKIP") {
-      var confirmSkip = window.confirm("Are you sure you want to skip?");
-      if (confirmSkip) {
-        window.alert(playerInfo.name + " has choosen to skip the fight!");
-        playerInfo.money = Math.max(0, playerInfo.money - 10);
-        console.log("playerInfo.money", playerInfo.money);
-        break;
-      }
+    if (fightOrSkip()) {
+      break;
     }
     var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
     enemy.health = Math.max(0, enemy.health - damage);
@@ -122,7 +135,6 @@ var startGame = function () {
   for (var i = 0; i < enemyInfo.length; i++) {
     if (playerInfo.health > 0) {
       window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
-      debugger;
 
       var pickedEnemyObj = enemyInfo[i];
       pickedEnemyObj.health = randomNumber(40, 60);
